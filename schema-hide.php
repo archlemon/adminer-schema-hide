1c3f6da61fcbb79d9a50a0745c1909be146d6a0f
Matt Edmonston <matt.edmonston@gmail.com>
2018-09-04 14:17:59 +0100

    Create schema-hide.php

---
+++ schema-hide.php
@@ -0,0 +1,27 @@
+<?php
+
+/** Hide some schemas
+* @link https://www.adminer.org/plugins/#use
+* @author Mett Edmonston, https://github.com/archlemon
+* @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+*/
+class AdminerSchemaHide {
+    protected $disabled;
+
+    /**
+    * @param array case insensitive schema names in values
+    */
+    function __construct($disabled = []) {
+        $this->disabled = array_map('strtolower', $disabled);
+    }
+
+    function schemas() {
+        $return = [];
+        foreach (schemas() as $schema) {
+            if (!in_array(strtolower($schema), $this->disabled)) {
+                $return[] = $schema;
+            }
+        }
+        return $return;
+    }
+}
